470f6a0e3544b2fb9946f2fffe1b85b0786b33f2
Dave Boling <daviddboling@gmail.com>
2014-04-12 16:47:32 -0400

    4/12/14 - Added some changes

--- js/handle.js
+++ js/handle.js
@@ -2,25 +2,69 @@ $(document).ready(function () {
 
 
 
+//ISOTOPE CONTROLS
 var content = $('#content').isotope({
 	filter: '.about_me',
 	itemSelector: '.item',
 	layoutMode: 'masonry',
 });
 
+
+//FADE-IN ON THE FIRST LOAD
 content.hide();
 content.fadeIn(1500);
 
 
+//Filtering System for ISOTOPE
 $('.filter .col-xs-6').on( 'click', 'div', function() {
     var filterValue = $( this ).attr('data-filter');
     content.isotope({filter: filterValue});
 
-    console.log('I was clicked!');
 
     $('div').children().removeClass('active');
 	$(this).addClass('active');
 
+    window.scrollTo(0, 0);
+
   });
+
+$('.top_content .backdrop .sub_content').on( 'click', 'div', function() {
+    var filterValue = $( this ).attr('data-filter');
+    content.isotope({filter: filterValue});
+
+
+    $('div').children().removeClass('active');
+	$(this).addClass('active');
+
+    window.scrollTo(0, 0);
+
+  });
+
+$('.contact_me .sub_content').on( 'click', 'div', function() {
+    var filterValue = $( this ).attr('data-filter');
+    content.isotope({filter: filterValue});
+
+
+    $('div').children().removeClass('active');
+    $(this).addClass('active');
+
+    window.scrollTo(0, 0);
+
+  });
+
+
+
+/* Top Menu Positioning */
+
+var num = 50; //number of pixels before modifying styles
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+        $('.filter').addClass('fixed');
+    } else {
+        $('.filter').removeClass('fixed');
+    }
+});
+
 
 });
